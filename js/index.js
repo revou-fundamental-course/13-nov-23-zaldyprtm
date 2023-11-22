834996dc5ba59e429bd55889165b3392aa3c6479
zaldyprtm <zaldyprtm@gmail.com>
2023-11-22 11:51:08 +0700

    ubah js

--- js/index.js
+++ js/index.js
@@ -14,23 +14,24 @@ function validateForm() {
 }
 
 let slideIndex = 1;
-showSlides(slindeIndex);
+showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides(n)  {
+        showSlides(slideIndex += n);
 }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName('image-slide');
- 
-    if(n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+function showSlides(n)  {
+    var i;
+    let slides = document.getElementsByClassName("mySlide");
+    if (n > slides.length) {slideIndex = 1}
+   else if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+       slides[i].style.display = 'none';
     }
-
     
-    slides[slideIndex-1].style.display = "block";
 
+    slides[slideIndex - 1].style.display = "block";
+    
 }
+
