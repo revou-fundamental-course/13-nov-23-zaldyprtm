1bb205637d2965ab98047654e8a6eb91a770e72a
zaldyprtm <zaldyprtm@gmail.com>
2023-11-22 10:40:41 +0700

    add js auto image

--- js/index.js
+++ js/index.js
@@ -9,5 +9,28 @@ function validateForm() {
         document.getElementById("errorSelect").innerHTML = "*Kamu belum memilih opsi"
 
         return false;
+        
     }
+}
+
+let slideIndex = 1;
+showSlides(slindeIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('image-slide');
+ 
+    if(n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    
+    slides[slideIndex-1].style.display = "block";
+
 }
